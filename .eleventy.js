e61b44618dacaadfdda20762b37ce6bb431b861b
Erik Näslund <code@nsld.dev>
2024-06-18 14:27:33 +0200

    Update layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,4 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addFilter("formatDate", function formatDate(date) {
-    const d = new Date(date);
-    return d.toLocaleDateString("en-EN", {
-      year: "numeric",
-    });
-  });
-
   eleventyConfig.addPassthroughCopy("normalize.css");
   eleventyConfig.addPassthroughCopy("bundle.css");
 };
